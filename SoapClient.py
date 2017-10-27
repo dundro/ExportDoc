ad2276e5f7087a5dcc46a8f38721968cdd5c6386
dundro <v.a.popov@outlook.com>
2017-10-27 17:16:29 +0300

    load all files instead of the first one

--- SoapClient.py
+++ SoapClient.py
@@ -1,45 +1,70 @@
 import datetime
 import json
+import multiprocessing
 from zeep import Client
 
 
 def sysdate():
-    return str(datetime.datetime.now())
+    return datetime.datetime.now().strftime('%d%m%Y_%H%M%S')
 
 
 def Init_Log():
-    File_Name = Output_Folder + sysdate() + '.log'
+    File_Name = sysdate() + '.log'
     return open(File_Name, 'w')
 
 
-def Log(Message, isSuccess, Id):
+def Log(Message, isSuccess, Id, Method, *args):
     Status = "SUCCESS" if isSuccess else "ERROR"
-    print(sysdate(), Status, Message, Id, sep=';', file=Log_File)
+    print(multiprocessing.current_process, sysdate(),
+          Status, Method, Message, Id, ';'.join(args), sep=';', file=Log_File)
     Log_File.flush()
 
-def SaveAttachmentByApplicationId(Application_Id, Siebel_Attachment_Service):
+
+def SaveAttachmentByApplicationId(Application_Id):
 
     try:
         Siebel_Response = Siebel_Attachment_Service.getOptyData(
             IntegrationId=Application_Id, FileType='Page Passport 2')
-        File_To_Save = Siebel_Response.Oppty.Doc[0].FileBuffer
-        File_Extension = Siebel_Response.Oppty.Doc[0].FileExt
-        outputfile = open(Output_Folder + Application_Id +
-                          '.' + File_Extension, 'wb')
-        outputfile.write(File_To_Save)
-        outputfile.close()
-        Log("file successfully saved", True, Application_Id)
-    except AttributeError:
-        Log("no attachment found", False, Application_Id)
     except BaseException as err:
-        Log(err, False, Application_Id)
+        print(err)
+        Log(err, False, Application_Id, 'SaveAttachmentByApplicationId')
+        outputfile = open('./Files/Log/' + Application_Id +
+                          '_response.xml', 'w')
+        outputfile.write(str(Siebel_Response))
+    else:
+        try:
+            for idx, doc in enumerate(Siebel_Response.Oppty.Doc):
+                Data_To_Be_Saved = doc.FileBuffer
+                File_Extension = doc.FileExt
+                Output_FileName = Application_Id + '_' + str(idx)+ '.' + File_Extension
+                # print('found file'+doc.FileName)
+                outputfile = open(Output_Folder + Output_FileName, 'wb')
+                outputfile.write(Data_To_Be_Saved)
+                Log("file successfully saved", True,
+                    Application_Id, 'SaveAttachmentByApplicationId', Output_FileName)
+        except AttributeError:
+            Log("no attachment found", False, Application_Id,
+                'SaveAttachmentByApplicationId')
+            outputfile = open(
+                './Files/Log/' + Application_Id + '_response.xml', 'w')
+            outputfile.write(str(Siebel_Response))
+        except BaseException as err:
+            print('Error occured \n')
+            print(err)
+            Log(err, False, Application_Id, 'SaveAttachmentByApplicationId')
+            outputfile = open('./Files/Log/' + Application_Id +
+                              '_response.xml', 'w')
+            outputfile.write(str(Siebel_Response))
+    finally:
+        outputfile.close()
 
 
-def GetApplicationsArray():
+def GetApplicationsArray(Input_Path):
     try:
-        Input_File = open(Input_File_Path, 'r')
+        Input_File = open(Input_Path, 'r')
     except FileNotFoundError:
-        Log("File " + Input_File_Path + " not found", False, Application_Id)
+        Log("File " + Input_Path + " not found", False,
+            Application_Id, 'GetApplicationsArray')
         return []
     else:
         ApplicationsArray = []
@@ -49,26 +74,33 @@ def GetApplicationsArray():
         return ApplicationsArray
 
 
-def main():
+def popAppfromList(Application_Id):
+    SaveAttachmentByApplicationId(
+        Application_Id)
 
+
+def main(Input_Path):
+    arr = GetApplicationsArray(Input_Path)
+    # for app in arr:
+    #     popAppfromList(app)
+    with multiprocessing.Pool(5) as p:
+        p.map(popAppfromList, arr)
+
+
+if __name__ == '__main__':
+    try:
+        with open('options.json') as data_file:
+            options = json.load(data_file)
+    except FileNotFoundError:
+        print("Options file doesn't exist")
+    Siebel_WS_Endpoint = options["Service_Endpoint"]
+    Input_File_Path = options["Input_File_Path"]
+    Output_Folder = options["Output_Folder"]
     client = Client(
         'wsdl/GetOpty.wsdl')
     Siebel_Attachment_Service = client.create_service('{http://siebel.com/CustomUI}ATC_spcGet_spcOpportunity_spcData_spcContactSys',
                                                       Siebel_WS_Endpoint)
-    for Application_Id in GetApplicationsArray():
-        SaveAttachmentByApplicationId(
-            Application_Id, Siebel_Attachment_Service)
-
-
-try:
-    with open('options.json') as data_file:
-        options = json.load(data_file)
-except FileNotFoundError:
-    print("Options file doesn't exist")
-Siebel_WS_Endpoint = options["Service_Endpoint"]
-Input_File_Path = options["Input_File_Path"]
-Output_Folder = options["Output_Folder"]
-Log_File = Init_Log()
-Log('options loaded successfully;' + str(options), True, '')
-main()
-Log_File.close()
+    Log_File = Init_Log()
+    # Log('options loaded successfully;' + str(options), True, '')
+    main(Input_File_Path)
+    Log_File.close()
